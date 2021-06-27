0d51d5d358dd7a702fb20cb6f6547ffe5961a2d5
alexandermed <alexandermed@mail.ru>
2021-06-27 03:35:29 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+
+hulk_url = "https://superheroapi.com/api/2619421814940190/search/hulk"
+lis = requests.get(hulk_url).json()['results']
+
+
+class YaUploader:
+    def __init__(self, file_list):
+        self.file_list = file_list
+
+    def upload(self):
+        """Метод загружает файлы по списку file_list на яндекс диск"""
+        # Тут ваша логика
+        url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        token = 'AQAAAAA9Vi2rAADLWzCWBMYbGUIDs3nuZ_W6Ies'
+        headers = {"Authorization": token}
+
+        for file_path in self.file_list:
+            params = {"path": file_path}
+            resp = requests.get(url, headers=headers, params=params)
+            print(resp.json())
+
+            with open(file_path, 'rb') as f:
+                print('Загрузка файла:', file_path)
+                response = requests.post(resp.json()['href'], files={"file": f})
+                print(str(response))
+                if str(response) == '<Response [201]>':
+                    print('Файл успешно загружен')
+                else:
+                    return 'Файл не загружен'
+
+        return 'Загрузка выполнена'
+
+
+
+
+if __name__ == '__main__':
+    uploader = YaUploader(["Файл для загрузки 1.txt", "Файл для загрузки 2.txt"])
+    result = uploader.upload()
+    print(result)
